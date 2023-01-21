dac03ca813b1407e4b681462f8127f2c51f22599
dudubfjj <dudubfjj@gmail.com>
2023-01-21 08:48:50 -0300

    testemain

--- main-off.py
+++ main-off.py
@@ -2,10 +2,16 @@ import discord
 from discord.ext import commands
 from discord.ext.commands.errors import MissingRequiredArgument, CommandNotFound, CommandInvokeError
 from decouple import config
+from time import sleep
 import requests
 
 import datetime
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ChromeOptions
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 import http.client
 import json
@@ -59,6 +65,28 @@ async def send_time(ctx):
 #GAMERSCLUB LAST GAME
 @client.command(name='gc', help="Digite o id da GamersClub após o comando. Ex: !gc 322861")
 async def get_url(ctx, id_gc):
+    cookies = {
+    '_gcl_au': '1.1.209042485.1667412074',
+    '_tt_enable_cookie': '1',
+    '_ttp': '07c8a5b2-ddbe-4b57-8db6-68428cd18630',
+    'language': 'pt-br',
+    'sib_cuid': '69ba150b-aa6a-481d-b287-e81cb7879e92',
+    '_hjSessionUser_2263196': 'eyJpZCI6IjM5YzhlYjVjLTcyODctNWNlMy1hZmMzLWFhOWVlNDVmMGY1MSIsImNyZWF0ZWQiOjE2Njc0MTIxNTgyNTIsImV4aXN0aW5nIjp0cnVlfQ==',
+    '_hjMinimizedPolls': '864076',
+    '_hjSessionUser_1963917': 'eyJpZCI6IjU4N2MzMzlkLTJkMWUtNTlmNS1iNzE2LTZmODljMmU1YTZmMyIsImNyZWF0ZWQiOjE2Njc0MTIwNzQ1MjEsImV4aXN0aW5nIjp0cnVlfQ==',
+    '_gid': 'GA1.3.195961545.1673814267',
+    '_hjDonePolls': '864076%2C865658%2C872356%2C873600',
+    'gclubsess': 'dc5fe6cfa591881570c87e2c0cc989ec16fe9f16',
+    '_hjIncludedInSessionSample': '0',
+    '_hjSession_2263196': 'eyJpZCI6ImEzZWNlZjAxLWVjZDEtNDM5Ny1hNjZjLWM4NjQyZjM0MGU5YiIsImNyZWF0ZWQiOjE2NzQyMzM1MDM1NTAsImluU2FtcGxlIjpmYWxzZX0=',
+    '_hjAbsoluteSessionInProgress': '0',
+    '_hjHasCachedUserAttributes': 'true',
+    '__cf_bm': 'FaSi7Qur_bC7Ms8oMjKw3w38XsPQ7lD1aHfcT2g0Jj0-1674233353-0-Af90vhBup0uUKQMqGrOeK2c+x6QBLT/Uo3So7cPdLMGymPvjLdtSpDCLYcAT4wTGjiwfN69KpEVqM40YfXGYijBhTIHWdTBj6JYFTR8sMXiob7FWlI4xHAnoavMGhRuADdbSb+ReLAvsNbyCQdNBXfg=',
+    '_ga': 'GA1.3.159586927.1667412074',
+    'SL_C_23361dd035530_VID': 'LFlRfyq_0v',
+    'SL_C_23361dd035530_KEY': 'a14d3638cda988422792e3613234743b983fdd9e',
+    '_ga_H7ETJY03DT': 'GS1.1.1674233503.232.1.1674234592.60.0.0',
+    '_ga_GDBFGFR1PC': 'GS1.1.1674233503.232.1.1674234592.60.0.0'}
 
     headers = {
         'authority': 'gamersclub.com.br',
@@ -73,7 +101,7 @@ async def get_url(ctx, id_gc):
         'sec-fetch-site': 'same-origin',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'}
 
-    response = requests.get(f'https://gamersclub.com.br/api/box/init/{id_gc}', headers=headers)
+    response = requests.get(f'https://gamersclub.com.br/api/box/init/{id_gc}', cookies=cookies, headers=headers)
     stat = response.json()
 
 
@@ -141,6 +169,37 @@ async def get_url(ctx, id_gc):
     for n, d in zip(nome, dado):
         await ctx.send(f'{n} = {d}')
 
+#INFORMANDO LEVEL E PONTOS
+@client.command(name='level', help='!level id para ver o level atual e quantos pontos faltam para subir.')
+async def get_level(ctx, id_gc):
+    options = ChromeOptions()
+    options.headless = True
+    navegador = webdriver.Chrome(options=options)
+    navegador.get(f'https://gamersclub.com.br/player/{id_gc}')
+  
+    #PEGANDO NICKNAME
+    nickname = navegador.find_element(By.CLASS_NAME, 'gc-profile-user-name').text
+
+    level_now = int(navegador.find_element(By.CLASS_NAME, 'PlayerLevel__text').text)
+    if level_now < 21:
+        next_level = level_now + 1
+    rating = int(navegador.find_element(By.CLASS_NAME, 'StatsBoxRating__Score').text)
+    if level_now < 20:
+        max_rating = int(navegador.find_element(By.CLASS_NAME, 'StatsBoxProgressBar__maxRating').text)
+    elif level_now >= 20:
+        max_rating = 0
+    
+    #PONTOS QUE FALTAM PRA SUBIR DE LEVEL
+    pontos_faltam = max_rating - rating
+
+    if level_now < 20:
+        await ctx.send(f'{nickname} atualmente é level {level_now} e faltam {pontos_faltam} pontos pra chegar no level {next_level}.')
+    elif level_now >= 20 and level_now != 99:
+        await ctx.send(f'{nickname} atualmente é level {level_now} com {rating} pontos. ')
+    elif level_now == 99:
+        await ctx.send(f'{nickname} atualmente é level {level_now} com {rating} pontos. O {nickname} jogou MAJOR! Tem que respeitar! ')
+
+    navegador.quit()
 
 #BANCO DE DADOS DE ID'S - GC
 @client.command(name='showids', help="!showids mostra o nosso cadastro de id's da GamersClub")
